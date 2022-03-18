e0a61134ce801d956507e1763c9a4bf380f639ba
Adiel Ben Meir <adiel1892@gmail.com>
2022-03-18 14:01:15 +0200

    implementing Notebook

---
+++ Notebook.hpp
@@ -0,0 +1,13 @@
+#include "Direction.hpp"
+#include <iostream>
+#include <string>
+using namespace std;
+namespace ariel{
+    class Notebook{
+        public:
+        void write(unsigned int page, unsigned int row, unsigned int column, Direction d , string str);
+        string read(unsigned int page, unsigned int row, unsigned int column, Direction d ,unsigned int length);
+        void erase(unsigned int page, unsigned int row, unsigned int column,Direction d ,unsigned int length);
+        void show(unsigned int page);
+    };
+}
