0ccc1925b21f5f6a53b4c007d4ee9d051b3d7705
Adiel Ben Meir <adiel1892@gmail.com>
2022-03-18 14:49:20 +0200

    change

--- Notebook.hpp
+++ Notebook.hpp
@@ -4,6 +4,7 @@
 using namespace std;
 namespace ariel{
     class Notebook{
+        private:
         public:
         void write(unsigned int page, unsigned int row, unsigned int column, Direction d , string str);
         string read(unsigned int page, unsigned int row, unsigned int column, Direction d ,unsigned int length);
