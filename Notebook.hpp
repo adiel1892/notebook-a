fced4775825c5b32a2d3d2f8198253b25c426dae
Adiel Ben Meir <adiel1892@gmail.com>
2022-03-21 19:34:22 +0200

    change

--- Notebook.hpp
+++ Notebook.hpp
@@ -7,7 +7,7 @@ namespace ariel{
     class Notebook{
         private:
         public:
-        static void write(unsigned int page, unsigned int row, unsigned int column, Direction d , string str);
+        static void write(unsigned int page, unsigned int row, unsigned int column, Direction d ,string const str);
         static string read(unsigned int page, unsigned int row, unsigned int column, Direction d ,unsigned int length);
         static void erase(unsigned int page, unsigned int row, unsigned int column,Direction d ,unsigned int length);
         void show(unsigned int page);
