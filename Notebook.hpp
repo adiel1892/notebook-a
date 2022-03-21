9783d341a75be79484bd5011108d68a9ff5bc863
Adiel Ben Meir <adiel1892@gmail.com>
2022-03-21 11:35:32 +0200

    bash grade -100. clang tidy not perfect

--- Notebook.hpp
+++ Notebook.hpp
@@ -1,14 +1,15 @@
 #include "Direction.hpp"
 #include <iostream>
 #include <string>
+#include <stdexcept>
 using namespace std;
 namespace ariel{
     class Notebook{
         private:
         public:
-        void write(unsigned int page, unsigned int row, unsigned int column, Direction d , string str);
-        string read(unsigned int page, unsigned int row, unsigned int column, Direction d ,unsigned int length);
-        void erase(unsigned int page, unsigned int row, unsigned int column,Direction d ,unsigned int length);
+        static void write(unsigned int page, unsigned int row, unsigned int column, Direction d , string str);
+        static string read(unsigned int page, unsigned int row, unsigned int column, Direction d ,unsigned int length);
+        static void erase(unsigned int page, unsigned int row, unsigned int column,Direction d ,unsigned int length);
         void show(unsigned int page);
     };
 }
