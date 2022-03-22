59b35d3d401fe68ef99382b1533f55aa28239b62
Adiel Ben Meir <adiel1892@gmail.com>
2022-03-22 12:23:50 +0200

    final

--- Test.cpp
+++ Test.cpp
@@ -34,10 +34,11 @@ TEST_CASE("Good Notebook"){
     CHECK(a.read(0,0,0,Direction::Horizontal,22) == "first line of notebook");
     CHECK(a.read(0,0,0,Direction::Horizontal,25) == "first line of notebook___");
     a.erase(0,0,0,Direction::Horizontal,6);
-    CHECK(a.read(0,0,0,Direction::Horizontal,25) == "~~~~~~line~~~~notebook___");
+    CHECK(a.read(0,0,0,Direction::Horizontal,25) == "~~~~~~line of notebook___");
     a.erase(0,0,10,Direction::Horizontal,4);
-    a.write(0,1,0,Direction::Vertical,"i'm writing vertival"); 
-    CHECK(a.read(0,0,0,Direction::Vertical,22) == "_i'm writing vertival_");
+    CHECK(a.read(0,0,0,Direction::Horizontal,25) == "~~~~~~line~~~~notebook___");
+    a.write(0,1,0,Direction::Vertical,"i'm writing vertical"); 
+    CHECK(a.read(0,0,0,Direction::Vertical,22) == "_i'm writing vertical_");
     CHECK(a.read(0,1,0,Direction::Horizontal,5) == "i____");
     CHECK(a.read(0,3,0,Direction::Horizontal,5) == "m____");
     CHECK(a.read(0,12,0,Direction::Horizontal,5) == " ____");
@@ -56,20 +57,20 @@ TEST_CASE("Good Notebook"){
 
 TEST_CASE("writing on a writen or deleaten place is illegal"){
     Notebook a;
-    CHECK(a.read(0,0,0,Direction::Horizontal,22) == "______________________");
-    a.write(0,0,0,Direction::Horizontal,"first line of notebook"); 
-    CHECK_THROWS(a.write(0,0,10, Direction::Vertical , "checking"));
-    CHECK_THROWS(a.write(0,0,3, Direction::Vertical , "checking"));
-    CHECK_THROWS(a.write(0,0,6, Direction::Vertical , "checking"));
-    CHECK_THROWS(a.write(0,0,2, Direction::Vertical , "checking"));
-    CHECK_THROWS(a.write(0,0,0, Direction::Vertical , "checking"));
-    CHECK_THROWS(a.write(0,0,8, Direction::Vertical , "checking"));
-    a.write(0,4,0,Direction::Horizontal,"second line of notebook"); 
-    CHECK_THROWS(a.write(0,3,0, Direction::Vertical , "aba"));
-    CHECK_THROWS(a.write(0,3,10, Direction::Vertical , "aba"));
-    CHECK_THROWS(a.write(0,3,4, Direction::Vertical , "aba"));
-    CHECK_THROWS(a.write(0,3,6, Direction::Vertical , "aba"));
-    CHECK_THROWS(a.write(0,3,5, Direction::Vertical , "aba"));
+    CHECK(a.read(1,0,0,Direction::Horizontal,22) == "______________________");
+    a.write(1,0,0,Direction::Horizontal,"first line of notebook"); 
+    CHECK_THROWS(a.write(1,0,10, Direction::Vertical , "checking"));
+    CHECK_THROWS(a.write(1,0,3, Direction::Vertical , "checking"));
+    CHECK_THROWS(a.write(1,0,6, Direction::Vertical , "checking"));
+    CHECK_THROWS(a.write(1,0,2, Direction::Vertical , "checking"));
+    CHECK_THROWS(a.write(1,0,0, Direction::Vertical , "checking"));
+    CHECK_THROWS(a.write(1,0,8, Direction::Vertical , "checking"));
+    a.write(1,4,0,Direction::Horizontal,"second line of notebook"); 
+    CHECK_THROWS(a.write(1,3,0, Direction::Vertical , "aba"));
+    CHECK_THROWS(a.write(1,3,10, Direction::Vertical , "aba"));
+    CHECK_THROWS(a.write(1,3,4, Direction::Vertical , "aba"));
+    CHECK_THROWS(a.write(1,3,6, Direction::Vertical , "aba"));
+    CHECK_THROWS(a.write(1,3,5, Direction::Vertical , "aba"));
 
 }
 TEST_CASE("(page || column || string || row) not valid"){
